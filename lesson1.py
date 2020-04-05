758c07667b5871c6b1bf0eda3647720ea8f76f5c
songb <weisong@gmail.com>
2020-04-05 09:35:35 -0400

    lesson1

---
+++ lesson1.py
@@ -0,0 +1,76 @@
+x = 5
+print(x)
+
+x*8
+
+x='test'
+print(x)
+
+x="hello"
+print(x)
+
+x*8
+
+x+10
+
+x+' 10'
+
+
+# list:
+
+x=[]
+x=[1,2,3]
+x[0]
+x[3]
+
+x.append(7)
+x.insert(1, 15)
+
+sum(x)
+len(x)
+
+for n in x:
+    print(n)
+
+x[1:3]
+
+x[1:]
+x[:3]
+
+x.append('test')
+
+x[-1]
+x[-2]
+
+x[-3:-1]
+
+x[-3:]
+
+
+# map/ dictionary
+
+person={'first':'john', 'last':'smith'}
+
+person['age'] = 30
+
+person[35] = 90
+
+person['x']
+
+person.keys()
+
+person.pop(35)
+
+
+
+# set
+
+s={1,2,2,3}
+
+s.add(8)
+
+# in (not in)
+
+# discard vs remove vs pop
+
+#   |(union)  &(intersection) -(difference)
---
+++ lesson2.py
@@ -0,0 +1,13 @@
+# for loop
+
+for i in range(10):
+    print('hello')
+    print(i)
+
+
+
+# list comprehension
+
+[x*2 for x in range(10)]
+
+[(x, y) for x in [1,2,3] for y in [3,1,4] if x != y]
